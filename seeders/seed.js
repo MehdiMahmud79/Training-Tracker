b5c76b4397969595a1af8cf7ad832a4b0de38739
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-10-23 22:37:30 +0100

    fix seed data

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../models');
+const db = require("../Models");
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
