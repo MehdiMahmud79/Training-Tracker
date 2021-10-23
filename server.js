be07b7175f7ffef5668591c7d8c5d8f58794a194
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-10-23 23:44:17 +0100

    update dbName

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 // create db connection to MongoDb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
