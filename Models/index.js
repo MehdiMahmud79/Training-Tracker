14f0e036ce7983e340a9faf2dc089bd50ba20821
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-10-23 22:37:48 +0100

    add workout Model

---
+++ Models/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number,
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = { Workout };
