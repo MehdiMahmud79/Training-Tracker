ac4e2c661c685f20097468166000afac6780b513
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-10-23 23:40:27 +0100

    add workout routes

---
+++ Controllers/apiRoutes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const { Workout } = require("../Models");
+
+router.get(`/`, async (req, res) => {
+  try {
+    const results = await Workout.find({});
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newWorkout = await Workout.create(req.body);
+    res.status(200).json(newWorkout);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const newExercise = await Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $push: { exercises: req.body },
+      },
+      { new: true }
+    );
+    res.status(200).json(newExercise);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.get(`/range`, async (req, res) => {
+  try {
+    var dbWorkouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+          totalWeight: { $sum: "$exercises.weight" },
+        },
+      },
+    ])
+      .sort({ day: -1 })
+      .limit(7);
+    res.status(200).json(dbWorkouts);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+module.exports = router;
