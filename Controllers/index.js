a2066107df12d1bbf79a614009444878eac453df
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-10-23 23:08:43 +0100

    update routes

--- Controllers/index.js
+++ Controllers/index.js
@@ -4,6 +4,6 @@ const apiRoutes = require("./apiRoutes");
 const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
+router.use("/api/workouts", apiRoutes);
 
 module.exports = router;
