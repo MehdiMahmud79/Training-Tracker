23b465cb6ad2cb606afa893c53fc28c02cf5caf2
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-10-23 23:08:55 +0100

    modefy server

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
 
 // create db connection to MongoDb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
